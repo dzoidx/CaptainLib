df938f15daa0f2e8c788ff7b249a638c359ee0de
morgan <dzoidx@gmail.com>
2019-03-05 23:45:41 +0300

    unit tests

---
+++ Collections.Tests/MemCacheTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CaptainLib.Collections;
+
+namespace Collections.Tests
+{
+    [TestClass]
+    public class MemCacheTests
+    {
+        [TestMethod]
+        public void TestNonexistingKeyInvalidate()
+        {
+            var cache = new MemCache<string, int>();
+
+            Assert.IsFalse(cache.Invalidate("notexists"));
+
+            cache.GetOrCreate("key", () => 0, TimeSpan.FromSeconds(1));
+            Assert.IsTrue(cache.Invalidate("key"));
+            Assert.IsFalse(cache.Invalidate("key"));
+        }
+    }
+}
